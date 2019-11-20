4b9bf13941d31af72e6cf59ede95efbbb6ca3776
Joel Frazier <frazieje@gmail.com>
2019-11-20 05:42:09 +0000

    create indexes

--- couchdbinit.sh
+++ couchdbinit.sh
@@ -6,6 +6,14 @@ curl -X PUT http://127.0.0.1:5984/_replicator
 
 curl -X PUT http://127.0.0.1:5984/_global_changes
 
+curl -X PUT http://127.0.0.1:5984/admins
+
+curl -H "Content-Type: application/json" \
+     -d "{\"language\":\"javascript\",\"views\":{\"by_email\":{\"map\":\"function (doc) {\n  emit(doc.email); \n}\"}}}" \
+     -X PUT http://127.0.0.1:5984/admins/_design/docs
+
 curl -X PUT http://127.0.0.1:5984/users
 
-curl -X PUT http://127.0.0.1:5984/admins
+curl -H "Content-Type: application/json" \
+     -d @usersview.json \
+     -X PUT http://127.0.0.1:5984/users/_design/docs
